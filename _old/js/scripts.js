5e815541df22fe8b7f9bc1e9a904b18d3aa75886
Oleksandr <lysenkoa86@gmail.com>
2018-09-25 15:35:53 +0300

    Commit

--- _old/js/scripts.js
+++ _old/js/scripts.js
@@ -54,7 +54,7 @@ const deposit = [
     {key: 'INK_USD', title: 'Ink', sum: 655, priceIn: 0.0228, priceMax: 0.915974},
     {key: 'XEM_USD', title: 'NEM', sum: 56.62, priceIn: 0.58, priceMax: 1.8892, priceNow: 0.11098},
     {key: 'XVG_USD', title: 'Verge', sum: 209, priceIn: 0.076, priceMax: 0.25177, priceNow: 0.01557},
-    {key: 'USD', title: 'Exmo', sum: 0, priceIn: 1, priceMax: 1, priceNow: 1}
+    {key: 'USD', title: 'Exmo', sum: 21.22, priceIn: 1, priceMax: 1, priceNow: 1}
 ];
 let totalSumIn = 0;
 let totalSumNow = 0;
