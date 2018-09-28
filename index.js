157429809bf784e8f872ffc55748c1c187c6af1a
Oleksandr <lysenkoa86@gmail.com>
2018-09-28 12:47:07 +0300

    Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public/build'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/templates/index.html');
