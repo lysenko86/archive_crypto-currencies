51af2b0d0c86002da3a2fffc1d62908a204b344f
Oleksandr <lysenkoa86@gmail.com>
2018-10-09 17:48:13 +0300

    Commit

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,15 @@ function roundPrice(price){
 }
 
 const deposit = [
-    {key: 'OMG_USD', title: 'OmiseGO', sum: 1.42628172, priceIn: 4.18, priceMax: 25.3071},
+    {key: 'LTC_USD', title: 'Litecoin', sum: 0.03657, priceIn: 60.69, priceMax: 352.616},
+    {key: 'LTC_USD', title: 'Litecoin', sum: 0.05, priceIn: 66.2, priceMax: 352.616},
+    {key: 'ZEC_USD', title: 'Zcash', sum: 0.03992, priceIn: 143.8788, priceMax: 731.37},
+    {key: 'EOS_USD', title: 'EOS', sum: 3.992, priceIn: 6.633, priceMax: 20.474},
+    {key: 'HBZ_USD', title: 'Helbiz', sum: 490.74, priceIn: 0.0038379, priceMax: 0.020609},
+    {key: 'DXT_USD', title: 'Datawallet', sum: 870, priceIn: 0.0115, priceMax: 0.99813},
+    {key: 'DXT_USD', title: 'Datawallet', sum: 515.254, priceIn: 0.0105, priceMax: 0.99813},
+    {key: 'DXT_USD', title: 'Datawallet', sum: 481, priceIn: 0.01053, priceMax: 0.99813},
+    {key: 'OMG_USD', title: 'OmiseGO', sum: 1.426, priceIn: 4.18, priceMax: 25.3071},
     {key: 'OMG_USD', title: 'OmiseGO', sum: 4.624, priceIn: 3.255, priceMax: 25.3071},
     {key: 'OMG_USD', title: 'OmiseGO', sum: 3, priceIn: 3.332, priceMax: 25.3071},
     {key: 'OMG_USD', title: 'OmiseGO', sum: 16.667, priceIn: 3.68, priceMax: 25.3071},
@@ -22,36 +30,32 @@ const deposit = [
     {key: 'OMG_USD', title: 'OmiseGO', sum: 5.551, priceIn: 3.53, priceMax: 25.3071},
     {key: 'OMG_USD', title: 'OmiseGO', sum: 14.642, priceIn: 3.55, priceMax: 25.3071},
     {key: 'OMG_USD', title: 'OmiseGO', sum: 14.403, priceIn: 3.55, priceMax: 25.3071},
-    {key: 'ADA_USD', title: 'Cardano', sum: 29.94, priceIn: 0.12, priceMax: 1.2655},
-    {key: 'ADA_USD', title: 'Cardano', sum: 117, priceIn: 0.0849, priceMax: 1.2655},
-    {key: 'XLM_USD', title: 'Stellar', sum: 0, priceIn: 0.244, priceMax: 0.87315},
-    {key: 'TRX_USD', title: 'Tron', sum: 0, priceIn: 0.023, priceMax: 0.27371},
-    {key: 'EOS_USD', title: 'EOS', sum: 3.992, priceIn: 6.633, priceMax: 20.474},
-    {key: 'GAS_USD', title: 'Gas', sum: 0, priceIn: 9.1, priceMax: 79.21},
-    {key: 'LTC_USD', title: 'Litecoin', sum: 0.03667, priceIn: 60.69, priceMax: 352.616},
-    {key: 'LTC_USD', title: 'Litecoin', sum: 0.05, priceIn: 66.2, priceMax: 352.616},
-    {key: 'ZEC_USD', title: 'Zcash', sum: 0.04, priceIn: 143.8788, priceMax: 731.37},
+    {key: 'ADA_USD', title: 'Cardano', sum: 31, priceIn: 0.12, priceMax: 1.2655},
+    {key: 'ADA_USD', title: 'Cardano', sum: 118.5, priceIn: 0.0849, priceMax: 1.2655},
+    {key: 'INK_USD', title: 'Ink', sum: 653.69, priceIn: 0.0228, priceMax: 0.915974},
     {key: 'NEO_USD', title: 'NEO', sum: 1.9896, priceIn: 24.07989, priceMax: 170.04},
-    {key: 'HBZ_USD', title: 'Helbiz', sum: 500, priceIn: 0.0038379, priceMax: 0.020609},
-    {key: 'STQ_USD', title: 'Storiqa', sum: 0, priceIn: 0.0022, priceMax: 0.013205},
-    {key: 'DXT_USD', title: 'Datawallet', sum: 870, priceIn: 0.0115, priceMax: 0.99813},
-    {key: 'DXT_USD', title: 'Datawallet', sum: 518, priceIn: 0.0105, priceMax: 0.99813},
-    {key: 'DXT_USD', title: 'Datawallet', sum: 481.55, priceIn: 0.01053, priceMax: 0.99813},
-    {key: 'INK_USD', title: 'Ink', sum: 655, priceIn: 0.0228, priceMax: 0.915974},
-    {key: 'MNX_USD', title: 'MinexCoin', sum: 0, priceIn: 2.268, priceMax: 51.633},
-    {key: 'BCH_USD', title: 'Bitcoin Cash', sum: 0, priceIn: 478.7, priceMax: 3446},
-    {key: 'XMR_USD', title: 'Monero', sum: 0, priceIn: 106.85, priceMax: 439.33},
 
-    {key: 'BTC_USD', title: 'Bitcoin', sum: 0.003729, priceIn: 11250, priceMax: 19331},
-    {key: 'XRP_USD', title: 'Ripple', sum: 14.9835297, priceIn: 1.1367, priceMax: 3.0515},
-    {key: 'XRP_USD', title: 'Ripple', sum: 40.09, priceIn: 1.125, priceMax: 3.0515},
-    {key: 'ETH_USD', title: 'Ethereum', sum: 0.031936, priceIn: 970, priceMax: 1391},
+    {key: 'BTC_USD', title: 'Bitcoin', sum: 0.0037293, priceIn: 11250, priceMax: 19331},
     {key: 'DASH_USD', title: 'Dash', sum: 0.01, priceIn: 740, priceMax: 1470},
     {key: 'DASH_USD', title: 'Dash', sum: 0.00996, priceIn: 626.104, priceMax: 1470},
+    {key: 'ETH_USD', title: 'Ethereum', sum: 0.031936, priceIn: 970, priceMax: 1391},
+    {key: 'XRP_USD', title: 'Ripple', sum: 14.98352, priceIn: 1.1367, priceMax: 3.0515},
+    {key: 'XRP_USD', title: 'Ripple', sum: 40, priceIn: 1.125, priceMax: 3.0515},
     {key: 'ETC_USD', title: 'Ethereum Classic', sum: 1.0, priceIn: 33.47, priceMax: 44.546},
-    {key: 'ETC_USD', title: 'Ethereum Classic', sum: 0.594, priceIn: 29.044, priceMax: 44.546},
-    {key: 'XEM_USD', title: 'NEM', sum: 56.62, priceIn: 0.58, priceMax: 1.8892, priceNow: 0.11098},
-    {key: 'XVG_USD', title: 'Verge', sum: 209, priceIn: 0.076, priceMax: 0.25177, priceNow: 0.01557},
+    {key: 'ETC_USD', title: 'Ethereum Classic', sum: 0.59136, priceIn: 29.044, priceMax: 44.546},
+
+    {key: 'XMR_USD', title: 'Monero', sum: 0, priceIn: 106.85, priceMax: 439.33},
+    {key: 'BCH_USD', title: 'Bitcoin Cash', sum: 0, priceIn: 478.7, priceMax: 3446},
+    {key: 'BTG_USD', title: 'Bitcoin Gold', sum: 0, priceIn: 478.7, priceMax: 3446},
+    {key: 'STQ_USD', title: 'Storiqa', sum: 0, priceIn: 0.0022, priceMax: 0.013205},
+    {key: 'XLM_USD', title: 'Stellar', sum: 0, priceIn: 0.244, priceMax: 0.87315},
+    {key: 'MNX_USD', title: 'MinexCoin', sum: 0, priceIn: 2.268, priceMax: 51.633},
+    {key: 'TRX_USD', title: 'Tron', sum: 0, priceIn: 0.023, priceMax: 0.27371},
+    {key: 'GAS_USD', title: 'Gas', sum: 0, priceIn: 9.1, priceMax: 79.21},
+
+    {key: 'XEM_USD', title: 'NEM', sum: 56.62, priceIn: 0.58, priceMax: 1.8892, priceNow: 0.10976},
+    {key: 'XVG_USD', title: 'Verge', sum: 209, priceIn: 0.076, priceMax: 0.25177, priceNow: 0.01596},
+
     {key: 'USD', title: 'Exmo', sum: 0, priceIn: 1, priceMax: 1, priceNow: 1}
 ];
 let totalSumIn = 0;
@@ -140,5 +144,3 @@ $(function(){
         $('.table-currencies tfoot td.col-increase-out-percent').text(totalPercentOut + '%');
     });
 });
-
-console.log('GOOD');
