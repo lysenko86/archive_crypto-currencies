e8e741141287e1e200696ed350b440797f141233
Oleksandr <lysenkoa86@gmail.com>
2019-01-11 23:19:16 +0200

    Commit

--- _/public/js/table-footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class TableFooter extends Component{
-    render(){
-        return <table className="table table-bordered table-currencies"><tfoot></tfoot></table>;
-    }
-}
-
-export default TableFooter;
--- public/jsx/Currencies.jsx
+++ public/jsx/Currencies.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Currencies extends React.Component{
     render(){
-        return (<div>Currencies</div>)
+        return (<div className="page-currencies">
+            <table class="table table-bordered table-currencies"><thead><tr>
+                <th class="col-name">Валюта</th>
+                <th class="col-sum">Сума</th>
+                <th class="col-price-in">Ціна закупа, $</th>
+                <th class="col-sum-in">Сума закупа, $</th>
+                <th class="col-price-now">Ціна зараз, $</th>
+                <th class="col-sum-now">Сума зараз, $</th>
+                <th class="col-increase-now">Ріст, $</th>
+                <th class="col-increase-now-percent">Ріст, %</th>
+                <th class="col-price-max">Макс, $</th>
+                <th class="col-price-out">Ціна продажу, $</th>
+                <th class="col-sum-out">Сума продажу, $</th>
+                <th class="col-increase-out-percent">Ріст продажу, %</th>
+                <th class="col-scrollbar"></th>
+            </tr></thead></table>
+            <div class="table-currencies-container">
+                <table class="table table-bordered table-hover table-currencies"><tbody></tbody></table>
+            </div>
+            <table class="table table-bordered table-currencies"><tfoot></tfoot></table>
+        </div>)
     }
 }
 
