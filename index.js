484aa53e6acb197aab46257a014cd2134a50463c
Oleksandr <lysenko86@i.ua>
2019-01-17 03:29:09 +0200

    Commit

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
-}).listen(2001);
+}).listen(80);
 
-console.log('Server was started at http://127.0.0.1:2001/');
+console.log('Server was started at http://127.0.0.1:80/');
