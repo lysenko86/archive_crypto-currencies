4a486e03d8bdb2c1feeeb8df4f2a50664d089736
Oleksandr <lysenko86@i.ua>
2019-08-13 05:15:57 +0300

    Commit

--- public/jsx/analytics.js
+++ public/jsx/analytics.js
@@ -18,5 +18,5 @@ export default [
     {month: 'Травень 2019', add: 0, capitalBeginMonth: 642.63, capitalEndMonth: 445.92},
     {month: 'Червень 2019', add: 0, capitalBeginMonth: 445.92, capitalEndMonth: 352.23},
     {month: 'Липень 2019', add: 0, capitalBeginMonth: 352.23, capitalEndMonth: 323.42},
-    {month: 'Серпень 2019', add: 0, capitalBeginMonth: 323.42, capitalEndMonth: 0},
+    {month: 'Серпень 2019', add: 0, capitalBeginMonth: 323.42, capitalEndMonth: 323.42},
 ];
