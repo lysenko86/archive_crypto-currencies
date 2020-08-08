2a4aa464caa72d2ee3af87f914c899c67708907f
Oleksandr <lysenkoa86@gmail.com>
2020-08-08 16:08:49 +0300

    CryptoCurrencies - update deposit

--- public/jsx/deposit.js
+++ public/jsx/deposit.js
@@ -1,37 +1,32 @@
 export default [
 
 // TOP 10
-    {key: 'EOS_USD', title: 'EOS', top: 10, sum: 4.60496, priceIn: 6.633, priceMax: 20.474},
-    {key: 'LTC_USD', title: 'Litecoin', top: 10, sum: 0.22853, priceIn: 134.5, priceMax: 352.616},
-    {key: 'BCH_USD', title: 'Bitcoin Cash', top: 10, sum: 0.06668, priceIn: 457, priceMax: 3446},
+    {key: 'EOS_USD', title: 'EOS', top: 10, sum: 4.60708811, priceIn: 6.633, priceMax: 20.474},
+    {key: 'LTC_USD', title: 'Litecoin', top: 10, sum: 0.2285383, priceIn: 134.5, priceMax: 352.616},
+    {key: 'BCH_USD', title: 'Bitcoin Cash', top: 10, sum: 0.0667799, priceIn: 457, priceMax: 3446},
     {key: 'BTC_USD', title: 'Bitcoin', top: 10, sum: 0.0026993, priceIn: 11250, priceMax: 19331},
-    {key: 'XLM_USD', title: 'Stellar', top: 10, sum: 136.726, priceIn: 0.22, priceMax: 0.87315},
-    {key: 'XRP_USD', title: 'Ripple', top: 10, sum: 26.88352, priceIn: 1.1367, priceMax: 3.0515},
+    {key: 'XLM_USD', title: 'Stellar', top: 10, sum: 136.91780001, priceIn: 0.22, priceMax: 0.87315},
+    {key: 'XRP_USD', title: 'Ripple', top: 10, sum: 26.61135527, priceIn: 1.1367, priceMax: 3.0515},
     {key: 'ETH_USD', title: 'Ethereum', top: 10, sum: 0.031936, priceIn: 970, priceMax: 1391},
 
 // TOP 20
-    {key: 'TRX_USD', title: 'Tron', top: 20, sum: 1197, priceIn: 0.0252, priceMax: 0.27371},
-    {key: 'ADA_USD', title: 'Cardano', top: 20, sum: 400, priceIn: 0.077, priceMax: 1.2655},
-    {key: 'XMR_USD', title: 'Monero', top: 20, sum: 0.28513, priceIn: 108, priceMax: 439.337},
-    {key: 'XEM_USD', title: 'NEM', top: 20, sum: 296.4, priceIn: 0.103, priceMax: 1.8892},
-    {key: 'ZEC_USD', title: 'Zcash', top: 20, sum: 0.21124, priceIn: 143.8788, priceMax: 731.37},
-    {key: 'NEO_USD', title: 'NEO', top: 20, sum: 1.2626, priceIn: 24.07989, priceMax: 170.04},
-    {key: 'ETC_USD', title: 'Ethereum Classic', top: 20, sum: 0.90418, priceIn: 33.47, priceMax: 44.546},
-    {key: 'DASH_USD', title: 'Dash', top: 20, sum: 0.04085, priceIn: 740, priceMax: 1470},
+    {key: 'TRX_USD', title: 'Tron', top: 20, sum: 1199.28000741, priceIn: 0.0252, priceMax: 0.27371},
+    {key: 'ADA_USD', title: 'Cardano', top: 20, sum: 400.76435074, priceIn: 0.077, priceMax: 1.2655},
+    {key: 'XMR_USD', title: 'Monero', top: 20, sum: 0.28553858, priceIn: 108, priceMax: 439.337},
+    {key: 'XEM_USD', title: 'NEM', top: 20, sum: 296.82180001, priceIn: 0.103, priceMax: 1.8892},
+    {key: 'ZEC_USD', title: 'Zcash', top: 20, sum: 0.21148699, priceIn: 143.8788, priceMax: 731.37},
+    {key: 'NEO_USD', title: 'NEO', top: 20, sum: 1.26260234, priceIn: 24.07989, priceMax: 170.04},
+    {key: 'ETC_USD', title: 'Ethereum Classic', top: 20, sum: 0.90418053, priceIn: 33.47, priceMax: 44.546},
+    {key: 'DASH_USD', title: 'Dash', top: 20, sum: 0.04088743, priceIn: 740, priceMax: 1470},
 
 // TOP 40
-    {key: 'WAVES_USD', title: 'Waves', top: 40, sum: 11.13892, priceIn: 2.36, priceMax: 17.331},
-    {key: 'BTG_USD', title: 'Bitcoin Gold', top: 40, sum: 0.78244, priceIn: 39, priceMax: 394.946},
-    {key: 'LSK_USD', title: 'Lisk', top: 40, sum: 9.65804, priceIn: 3.15, priceMax: 35.592},
-    {key: 'OMG_USD', title: 'OmiseGO', top: 40, sum: 7.31634, priceIn: 4.18, priceMax: 25.3071},
-    {key: 'DOGE_USD', title: 'Dogecoin', top: 40, sum: 0, priceIn: 0.016702, priceMax: 1470},
-    {key: 'XTZ_USD', title: 'Tezos', top: 40, sum: 0, priceIn: 1.23797, priceMax: 8.10891},
-    {key: 'QTUM_USD', title: 'Qtum', top: 40, sum: 0, priceIn: 0, priceMax: 65.853},
+    {key: 'WAVES_USD', title: 'Waves', top: 40, sum: 11.13892014, priceIn: 2.36, priceMax: 17.331},
+    {key: 'BTG_USD', title: 'Bitcoin Gold', top: 40, sum: 0.78354231, priceIn: 39, priceMax: 394.946},
+    {key: 'LSK_USD', title: 'Lisk', top: 40, sum: 9.67159356, priceIn: 3.15, priceMax: 35.592},
+    {key: 'OMG_USD', title: 'OmiseGO', top: 40, sum: 7.3163461, priceIn: 4.18, priceMax: 25.3071},
 
 // TOP 150
     {key: 'GAS_USD', title: 'Gas', top: 150, sum: 5.6156, priceIn: 5.37, priceMax: 79.21},
-    {key: 'ZRX_USD', title: '0x', top: 150, sum: 0, priceIn: 0, priceMax: 2.3183},
-    {key: 'SMART_USD', title: 'SmartCash', top: 150, sum: 0, priceIn: 0, priceMax: 2.222},
 
-    {key: 'USD', title: 'Exmo', sum: 30.8, priceIn: 1, priceMax: 1, priceNow: 1}
+    {key: 'USD', title: 'Exmo', sum: 0, priceIn: 1, priceMax: 1, priceNow: 1}
 ];
