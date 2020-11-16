a5181c28dcc2bb409cfca8cc76f25f5241fdbdf9
Oleksandr <olysenko@reaktivate.com>
2020-11-16 11:47:11 +0200

    deposit

--- public/jsx/deposit.js
+++ public/jsx/deposit.js
@@ -1,16 +1,17 @@
 export default [
 
 // TOP 10
+    {key: 'BTC_USD', title: 'Bitcoin', top: 10, sum: 0.0026993, priceIn: 11250, priceMax: 19331},
     {key: 'EOS_USD', title: 'EOS', top: 10, sum: 4.60708811, priceIn: 6.633, priceMax: 20.474},
     {key: 'LTC_USD', title: 'Litecoin', top: 10, sum: 0.2285383, priceIn: 134.5, priceMax: 352.616},
     {key: 'BCH_USD', title: 'Bitcoin Cash', top: 10, sum: 0.0667799, priceIn: 457, priceMax: 3446},
-    {key: 'BTC_USD', title: 'Bitcoin', top: 10, sum: 0.0026993, priceIn: 11250, priceMax: 19331},
     {key: 'XLM_USD', title: 'Stellar', top: 10, sum: 136.91780001, priceIn: 0.22, priceMax: 0.87315},
     {key: 'XRP_USD', title: 'Ripple', top: 10, sum: 26.61135527, priceIn: 1.1367, priceMax: 3.0515},
     {key: 'ETH_USD', title: 'Ethereum', top: 10, sum: 0.031936, priceIn: 970, priceMax: 1391},
 
 // TOP 20
-    {key: 'TRX_USD', title: 'Tron', top: 20, sum: 1199.28000741, priceIn: 0.0252, priceMax: 0.27371},
+    {key: 'TRX_USD', title: 'Tron', top: 20, sum: 1199.28000741, priceIn: 0.0252, priceMax: 0.0927},
+    {key: 'TRX_USD', title: 'Tron', top: 20, sum: 1000.000, priceIn: 0.0252, priceMax: 0.0927},
     {key: 'ADA_USD', title: 'Cardano', top: 20, sum: 400.76435074, priceIn: 0.077, priceMax: 1.2655},
     {key: 'XMR_USD', title: 'Monero', top: 20, sum: 0.28553858, priceIn: 108, priceMax: 439.337},
     {key: 'XEM_USD', title: 'NEM', top: 20, sum: 296.82180001, priceIn: 0.103, priceMax: 1.8892},
@@ -20,7 +21,7 @@ export default [
     {key: 'DASH_USD', title: 'Dash', top: 20, sum: 0.04088743, priceIn: 740, priceMax: 1470},
 
 // TOP 40
-    {key: 'WAVES_USD', title: 'Waves', top: 40, sum: 11.13892014, priceIn: 2.36, priceMax: 17.331},
+    {key: 'WAVES_USD', title: 'Waves', top: 40, sum: 0, priceIn: 2.36, priceMax: 17.331},
     {key: 'BTG_USD', title: 'Bitcoin Gold', top: 40, sum: 0.78354231, priceIn: 39, priceMax: 394.946},
     {key: 'LSK_USD', title: 'Lisk', top: 40, sum: 9.67159356, priceIn: 3.15, priceMax: 35.592},
     {key: 'OMG_USD', title: 'OmiseGO', top: 40, sum: 7.3163461, priceIn: 4.18, priceMax: 25.3071},
@@ -28,5 +29,5 @@ export default [
 // TOP 150
     {key: 'GAS_USD', title: 'Gas', top: 150, sum: 5.6156, priceIn: 5.37, priceMax: 79.21},
 
-    {key: 'USD', title: 'Exmo', sum: 0, priceIn: 1, priceMax: 1, priceNow: 1}
+    {key: 'USD', title: 'Exmo', sum: 30, priceIn: 1, priceMax: 1, priceNow: 1}
 ];
