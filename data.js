3cdfd266b6213e82f1da3fbdc87b667685a0f4b8
Oleksandr <olysenko@reaktivate.com>
2021-02-01 15:33:56 +0200

    update deposit

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const deposit = [
 // TOP 5
     {key: 'topHead', title: 'TOP 5'},
-    {key: 'BTC_USD', title: 'Bitcoin',  top: 5, sum: 0.00282703, priceIn: 0, priceMin: 3500,   priceMax: 41000},
+    {key: 'BTC_USD', title: 'Bitcoin',  top: 5, sum: 0.00282703, priceIn: 33780, priceMin: 3500,   priceMax: 41000},
     {key: 'ETH_USD', title: 'Ethereum', top: 5, sum: 0.0000000,  priceIn: 0, priceMin: 105,    priceMax: 1250},
     {key: 'XRP_USD', title: 'Ripple',   top: 5, sum: 0.0000000,  priceIn: 0, priceMin: 0.205,  priceMax: 3.2},
     {key: 'LTC_USD', title: 'Litecoin', top: 5, sum: 0.0000000,  priceIn: 0, priceMin: 35,     priceMax: 320},
